14fcea6ef88e7928c763496897f68f56bd0db53a
pablogaraay <garayperezpablo@gmail.com>
2026-02-12 00:16:22 +0100

    mongodb definido

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
+  #Scraper config
   KEYWORDS = ["Deloitte", "Accenture", "KPMG", "EY", "Capgemini", "PwC", "Indra", "NTT Data", "BCG", "Kyndryl"]
   LINKEDIN_API = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search"
   LOCATION = "Spain"
+
+  #DB Config
+  MONGO_URI = os.getenv("MONGO_URI")
+  DB_NAME = "nexTalent"
---
+++ dbConn.py
@@ -0,0 +1,15 @@
+from config import Config
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+class MongoManager:
+  def __init__(self):
+    self.client = MongoClient(Config.MONGO_URI, server_api=ServerApi(version='1'))
+    self.db = self.client[Config.DB_NAME]
+
+  def verify_connection(self):
+    try:
+      self.client.admin.command('ping')
+      print("Ping exitoso")
+    except Exception as e:
+      print(f"Servidor no disponible: {e}")
